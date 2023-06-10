eff4b15b90702c7a1f1256d5bf9c8f7ac0b5488a
Nik230408 <nikitamoldovan03@gmail.com>
2023-06-10 13:32:12 +0300

    inital

---
+++ main.py
@@ -0,0 +1,17 @@
+'''def strcounter(str):N*M
+    for sym in set(str):
+        counter = 0
+        for sub_sym in str:
+            if sym == sub_sym:
+                counter += 1
+        print(sym, counter)
+strcounter('afaarjvb')'''
+
+def strcounter(str): #O(N)
+    syms_counter = {}
+    for sym in str:
+        syms_counter[sym] = syms_counter.get(sym,0) + 1
+    for sym, count in syms_counter.items():
+        print(sym,count)
+
+strcounter('avbgax')
