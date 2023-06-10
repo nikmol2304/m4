26750e7413d3eb4fbd778ac1e8d4a84aece85dc5
Nik230408 <nikitamoldovan03@gmail.com>
2023-06-10 13:35:52 +0300

    second version

--- main.py
+++ main.py
@@ -7,6 +7,15 @@
         print(sym, counter)
 strcounter('afaarjvb')'''
 
+'''def strcounter(str):N**2
+    for sym in str:
+        counter = 0
+        for sub_sym in str:
+            if sym == sub_sym:
+                counter += 1
+        print(sym, counter)
+strcounter('afaarjvb')'''
+
 def strcounter(str): #O(N)
     syms_counter = {}
     for sym in str:
