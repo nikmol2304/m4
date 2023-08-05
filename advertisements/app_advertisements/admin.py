d69c18204c1f8b5cff65a3c0311e8943a045feb6
Nik230408 <nikitamoldovan03@gmail.com>
2023-08-05 12:21:03 +0300

    Дз лекция 8

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction']
+    list_filter = ['auction', 'created_at']
+    actions = ['make_auction_as_false','make_auction_as_true']
+    fieldsets = (
+        (
+           'Общие', {
+             'fields' :('title', 'description')}
+        ),
+         (
+           'Финансы', {
+             'fields' :('price', 'auction'),
+             'classes' :['collapse']
+             }
+        ),
+    )
+
+    # У Выделенных обьектов заменяет значение 'торг' на 'нет'
+    @admin.action(description=' Убрать возможность торга')
+    def make_auction_as_false(self,request, queryset):
+        queryset.update(auction=False)
+    # У Выделенных обьектов заменяет значение 'торг' на 'да'
+    @admin.action(description=' Добавить возможность торга')
+    def make_auction_as_true(self,request, queryset):
+        queryset.update(auction=True)
+    
+admin.site.register(Advertisement, AdvertisementAdmin)
+
